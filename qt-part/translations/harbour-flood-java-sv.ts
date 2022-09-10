e32780e2948c98b3b0c0108c8ea706e2a86d7807
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-09-10 08:14:43 +0200

    Create harbour-flood-java-sv.ts
    Add Swedish translation.
    Thanks for this nice game.

---
+++ qt-part/translations/harbour-flood-java-sv.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Flood to Java</source>
+        <translation>Flood to Java</translation>
+    </message>
+</context>
+<context>
+    <name>GamePage</name>
+    <message>
+        <source>Start New Game</source>
+        <translation>Starta nytt spel</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <source>Amount of Colors</source>
+        <translation>Antal färger</translation>
+    </message>
+    <message>
+        <source>Enable 2 Player Mode</source>
+        <translation>Aktivera 2-spelarläge</translation>
+    </message>
+    <message>
+        <source>Game Size</source>
+        <translation>Spelstorlek</translation>
+    </message>
+    <message>
+        <source>Rules: No rules! Press the buttons below, flood the field, enjoy the game.&lt;br&gt;The ComboScore is the square of your newly flooded fields.</source>
+        <translation>Regler: Inga regler! Tryck på knapparna nedan, översvämma fältet, njut av spelet. &lt;br&gt;Kombinationspoäng är kvadraten på dina nyligen översvämmade fält.</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerScore</name>
+    <message>
+        <source>You won! </source>
+        <translation>Du vann! </translation>
+    </message>
+    <message>
+        <source>Steps: %1 ComboScore: %2</source>
+        <translation>Steg: %1 Kombinationspoäng: %2</translation>
+    </message>
+</context>
+</TS>
